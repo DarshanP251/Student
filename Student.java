0f06276d88290dcf1e66da96c33a8bcb402c0983
DarshanP251 <darshanpatil0511@gmail.com>
2025-04-29 11:28:36 +0530

    Saved

---
+++ Student.java
@@ -0,0 +1,13 @@
+package Student;
+
+public class Student {
+    public static void main(String[] args) {
+        String name = "Darshan";
+        String branch = "BCA";
+        String id = "01fe23bca307";
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Branch: " + branch);
+        System.out.println("ID: " + id);
+    }
+}
